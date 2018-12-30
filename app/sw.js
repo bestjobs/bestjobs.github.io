685c46498a9a2166ea9cbb6cab69b9ea5ef2902d
bestjobs <46110010+bestjobs@users.noreply.github.com>
2018-12-30 21:15:25 +0200

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,1 +1,19 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('bestjobs').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html'
+     ]);
+   })
+ );
+});
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
 
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
