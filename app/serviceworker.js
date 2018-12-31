963cf2dd09d78134d4dacc98dc3d4ff6566be1f1
bestjobs <46110010+bestjobs@users.noreply.github.com>
2018-12-31 18:39:37 +0200

    Update serviceworker.js

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -5,14 +5,23 @@
 *
 */
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('bestjobs').then(function(cache) {
-     return cache.addAll([
-       '/index.html'
-     ]);
-   })
- );
+const cacheName = `bestjobs`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/styles/main.css`,
+        `/scripts/main.min.js`,
+        `/scripts/comlink.global.js`,
+        `/scripts/messagechanneladapter.global.js`,
+        `/scripts/pwacompat.min.js`,
+        `/sounds/airhorn.mp3`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
@@ -21,7 +30,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open(bestjobs)
+    caches.open(cacheName)
       .then(cache => cache.match(event.request, {ignoreSearch: true}))
       .then(response => {
       return response || fetch(event.request);
