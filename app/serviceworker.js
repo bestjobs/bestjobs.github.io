ada2267dd9bea12979081e16bcc5a28b55d07190
bestjobs <46110010+bestjobs@users.noreply.github.com>
2018-12-31 19:13:52 +0200

    specifies only HTML files in the root
    specifies only HTML files in the root

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -10,7 +10,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        `/`
+        `/*.html`
       ])
           .then(() => self.skipWaiting());
     })
