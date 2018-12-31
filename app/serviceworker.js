f2284f786f3e9bf96b6db9a8b7b8d594379fa2c7
bestjobs <46110010+bestjobs@users.noreply.github.com>
2018-12-31 18:23:07 +0200

    Update serviceworker.js

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -21,7 +21,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open(cacheName)
+    caches.open(bestjobs)
       .then(cache => cache.match(event.request, {ignoreSearch: true}))
       .then(response => {
       return response || fetch(event.request);
