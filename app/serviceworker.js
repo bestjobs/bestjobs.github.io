1f47cd1637196d9166a0e2e37583690416b51ee1
bestjobs <46110010+bestjobs@users.noreply.github.com>
2018-12-31 18:42:14 +0200

    Update serviceworker.js

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -10,14 +10,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        `/`,
-        `/index.html`,
-        `/styles/main.css`,
-        `/scripts/main.min.js`,
-        `/scripts/comlink.global.js`,
-        `/scripts/messagechanneladapter.global.js`,
-        `/scripts/pwacompat.min.js`,
-        `/sounds/airhorn.mp3`
+        `/`
       ])
           .then(() => self.skipWaiting());
     })
