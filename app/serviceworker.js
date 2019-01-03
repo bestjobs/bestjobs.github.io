929cbb64624ee3c46ddc125357e03fea72d972ea
bestjobs <46110010+bestjobs@users.noreply.github.com>
2019-01-03 19:31:03 +0200

    Update serviceworker.js

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -3,19 +3,3 @@
  Copyright © 2018 bestjobs. All rights reserved!
 */
 
-var CACHE_NAME = 'bestjobs';
-var urlsToCache = [
-  '/',
-  '/*.html'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
